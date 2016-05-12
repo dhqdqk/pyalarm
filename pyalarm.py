c957d1ea107a9451a1fb18d82b447ba84ea31b64
duanhq <690200292@qq.com>
2016-05-13 07:22:06 +0800

    commmits updated; add work-rest time pairs

--- pyalarm.py
+++ pyalarm.py
@@ -14,120 +14,124 @@ from multiprocessing import Process
 
 time_work = 25 * 60 # 25min；工作周期时间; work time period
 time_rest = 5 * 60 # 5min；休息周期时间; rest time period
-#time_pause_day = [['12:00:00', '13:00:00'], ['19:00:00', '08:30:00']]
-#time_pause_night = [['06:00:00', '19:00:00'], ]
-time_effect = ['19:00:00', '07:00:00'] # 闹钟每天生效时间段; effective time of alarm every day;
+# 闹钟每天生效时间段; effective time of alarm every day;the first one should be start-time
+time_effect = ['19:00:00', '22:00:00', '23:00:00', '09:00:00']
 
 class Alarm(object):
     def __init__(self, time_effect, time_work, time_rest):
         '''
+        period: 一个工作周期
         alarm_state: 闹钟是否工作的标记
-        tip: 已生效闹钟是否已执行的标记；默认为True，打开程序时提醒一次。
+        tip: 已生效闹钟是否已执行的标记；默认为True，打开程序时提醒一次
         next_alarm_time: 下次有效闹钟的时间(UTC)
         state:标记当前是工作（Work）还是休息（Rest）状态
         now_year_month_day: 当前年月日时
         now_time：当前时分秒之时
         next_alarm_text：下次闹钟的时分秒之时
-        time_effect_len：程序闹钟有效工作的时间长度
-        next_effect_time:下次程序闹钟生效的时间起点
-        next_ineffect_time: 下次程序闹钟失效的时间起点
+        state_time_len：程序闹钟当前状态的时间长度
+        state_start:本次状态的时间起点
+        state_end: 本次状态的时间终点
         effect：标记程序闹钟是否生效
         pause：标记闹钟停止时已提醒（只提醒一次）
         '''
-        self.time_work = time_effect
-        self.time_active = time_work
+        self.time_effect = time_effect
+        self.time_effect_utc = []
+        self.time_work = time_work
         self.time_rest = time_rest
+        self.period = self.time_work + self.time_rest
         self.alarm_state = True
+        self.state_start = 0
+        self.state_end = 0
         self.tip = True
         self.next_alarm_time = 0
         self.state = ''
         self.now_year_month_day  = ''
         self.now_time = ''
         self.next_alarm_text = ''
-        self.time_effect_len = 0
-        self.next_effect_time = 0
-        self.next_ineffect_time = 0
+        self.state_time_len = 0
         self.effect = True
         self.pause = 0
+        self.zero_time_utc = 0
+        self.h24_time_utc = 0
     
-    def init_alarm(self):
+    def time_setting(self):
         now_year = time.strftime('%Y')
         now_month_day_en = time.strftime('%a %b %d')
+        zero = "00:00:00"
+        h24 = "23:59:59"
         
-        time_effect_start = time.mktime(time.strptime(now_month_day_en+' '+self.time_work[0]+' '+now_year, "%a %b %d %H:%M:%S %Y"))
+        if len(self.time_effect) % 2 == 0:
+            for i in self.time_effect:
+                self.time_effect_utc.append(time.mktime(time.strptime(now_month_day_en+' '+
+                    i+' '+now_year, "%a %b %d %H:%M:%S %Y")))
+        else:
+            raise Exception("time_effect only accepts start-end time pairs")
+        self.zero_time_utc = time.mktime(time.strptime(now_month_day_en+' '+
+                    zero+' '+now_year, "%a %b %d %H:%M:%S %Y"))
+        self.h24_time_utc = time.mktime(time.strptime(now_month_day_en+' '+
+                    h24+' '+now_year, "%a %b %d %H:%M:%S %Y")) + 1.0
         
-        time_effect_end = time.mktime(time.strptime(now_month_day_en+' '+self.time_work[1]+' '+now_year, "%a %b %d %H:%M:%S %Y"))
-        
-        self.time_effect_len = time_effect_end - time_effect_start
-        self.next_pause_time = time_effect_end
-        
+    def init_alarm(self):
+        self.time_setting()
         now_utctime = time.time()
+        mintime = []
+        for i, j in enumerate(self.time_effect_utc):
+            if now_utctime < j:    
+                mintime.append(j)
+        mintime.sort()
+        now_point = self.time_effect_utc.index(mintime[0])
+        self.state_start = self.time_effect_utc[now_point-1]
+        self.state_end = self.time_effect_utc[now_point]
         
-        if time_effect_end > time_effect_start:
-            # 当前时间在生效期间内
-            if (now_utctime >= time_effect_start) and (now_utctime < time_effect_end):
-                self.effect = True
-                self.next_effect_time = time_effect_start + 24 * 60 * 60
-                self.next_ineffect_time = time_effect_end
-                init_time = (now_utctime - time_effect_start) % 1800
-                
-                if init_time < self.time_active:
+        if i % 2 == 0:
+            self.effect = False
+            if self.state_start < self.state_end:
+                self.state_time_len = self.state_end - self.state_start
+            else:
+                self.state_time_len = 24 * 60 * 60 - (self.state_start - self.state_end)
+        else:
+            self.effect = True
+            if self.state_start < self.state_end:
+                self.state_time_len = self.state_end - self.state_start
+                space_time = (now_utctime - self.state_time_len) % self.period
+                if space_time < self.time_work:
                     # 若程序起动时，时间在工作时间段，将状态置为工作，计算下一次休息时间；
                     # 否则对应是休息状态和下次工作时间
                     # True-'work time'; False-' Rest time'
                     self.alarm_state = True
-                    self.next_alarm_time = 1800 - init_time + time.time() - self.time_rest
+                    self.next_alarm_time = self.period - space_time + time.time() - self.time_rest
                 else: 
                     self.alarm_state = False
-                    self.next_alarm_time = 1800 - init_time + time.time()
+                    self.next_alarm_time = self.period - space_time + time.time()
             else:
-                # 已在罢工期间
-                self.effect = False
-                if now_utctime < time_effect_start:
-                    self.next_effect_time = time_effect_start
+                self.state_time_len = 24 * 60 * 60 - (self.state_start - self.state_end)
+                if now_utctime < self.state_end:
+                    space_time = (self.state_time_len - (self.state_end - now_utctime)) % self.period
                 else:
-                    self.next_effect_time = time_effect_start + 24 * 60 * 60
-                self.next_ineffect_time = self.next_effect_time + self.time_effect_len
-                
-        else:               
-            if (now_utctime >=  time_effect_end) and (now_utctime <= time_effect_start):
-                # 已在罢工期间
-                self.effect = False
-                self.next_effect_time = time_effect_start + 24 * 60 * 60 - self.time_effect_len
-                self.next_ineffect_time = self.next_effect_time + self.time_effect_len
-            else:
-                # 当前时间在生效期间内
-                self.effect = True
-                self.next_effect_time = time_effect_start
-                if now_utctime < time_effect_end:
-                    self.next_ineffect_time = time_effect_end
-                    init_time = (self.time_effect_len - (time_effect_end - now_utctime)) % 1800
-                else:
-                    self.next_ineffect_time = self.next_effect_time + self.time_effect_len
-                    init_time = (now_utctime - time_effect_start) % 1800
-                    self.next_ineffect_time = self.next_effect_time + self.time_effect_len
-                
-                if init_time < self.time_active:
+                    space_time = (now_utctime - self.state_start) % self.period
+                if space_time < self.time_work:
                     self.alarm_state = True
-                    self.next_alarm_time = 1800 - init_time + time.time() - self.time_rest
+                    self.next_alarm_time = self.period - space_time + time.time() - self.time_rest
                 else: 
                     self.alarm_state = False
-                    self.next_alarm_time = 1800 - init_time + time.time()
-                
+                    self.next_alarm_time = self.period - space_time + time.time()
     
     def alarm(self):
         now_utctime = time.time()
         # 判断是否状态将改变，是则切换状态并计算下次切换时间
-        if self.next_pause_time - now_utctime < self.time_effect_len:
+        if now_utctime < self.state_end:
             if now_utctime - self.next_alarm_time == 0:
                 self.tip = False
                 if self.alarm_state:
                     self.alarm_state = False
-                    self.next_alarm_time += self.time_active
+                    self.next_alarm_time += self.time_work
                 else:
                     self.alarm_state = True
                     self.next_alarm_time += self.time_rest
-        
+        else:
+            self.init_alarm()
+    
+    def alarmMessage(self):
         #　显示年月日
         self.now_year_month_day = time.strftime('%Y-%m-%d')
         # 显示当前时间
@@ -136,8 +140,6 @@ class Alarm(object):
         self.state = 'Work' if self.alarm_state else 'Rest'
         # 下次提醒时间
         self.next_alarm_text = time.strftime("%H:%M:%S", time.localtime(self.next_alarm_time))
-    
-    def alarmMessage(self):
         if self.effect:
             message = self.now_year_month_day + "\n" + self.now_time + \
                             "\nWork State: " + self.state + "\nNext Alarm: " + \
@@ -145,7 +147,7 @@ class Alarm(object):
         else:
             message = "now alarm is inffective.\n" + \
                     "next effective time is:\n" + \
-                    time.strftime("%H:%M:%S", time.localtime(self.next_effect_time))
+                    time.strftime("%H:%M:%S", time.localtime(self.state_end))
         return message
 
 
@@ -163,9 +165,6 @@ def alarmGUI(message):
     #b = Tkinter.Button(mainwin, text='alarm', command=alarm_show)
     #b.pack(side=Tkinter.LEFT)
     #b.pack(expand=Tkinter.YES, fill=Tkinter.BOTH)
-    
-    #hi = Tkinter.Button(mainwin, text="hi", command=sayhi)
-    #hi.pack(side=Tkinter.LEFT)
     
     mainwin.mainloop()
 
